d7a9fe63276b3f3eb30f2eb60b3fcad94491950a
LIHA11 <LIHA11@OOCL.com>
2025-09-05 20:49:49 +0800

    Use meaningful names for variables and methods and No more than 4 if/else branches in a single method

--- src/test/java/tdd/fizzbuzz/MarsRover.java
+++ src/test/java/tdd/fizzbuzz/MarsRover.java
@@ -12,57 +12,75 @@ public class MarsRover {
     }
 
     public void move(String command) {
-        if (command.equals("move")) {
-            if (direction.equals(MarsDirectionEnum.N.name())) {
-                this.y += 1;
-            } else if (direction.equals(MarsDirectionEnum.S.name())) {
-                this.y -= 1;
-            } else if (direction.equals(MarsDirectionEnum.E.name())) {
-                this.x += 1;
-            } else if (direction.equals(MarsDirectionEnum.W.name())) {
-                this.x -= 1;
-            }
+        switch (command) {
+            case "move":
+                moveForward();
+                break;
+            case "back":
+                moveBackward();
+                break;
+            case "left":
+                turnLeft();
+                break;
+            case "right":
+                turnRight();
+                break;
+            default:
         }
-
-        if(command.equals("back")){
-            if (direction.equals(MarsDirectionEnum.N.name())) {
-                this.y -= 1;
-            } else if (direction.equals(MarsDirectionEnum.S.name())) {
-                this.y += 1;
-            } else if (direction.equals(MarsDirectionEnum.E.name())) {
-                this.x -= 1;
-            } else if (direction.equals(MarsDirectionEnum.W.name())) {
-                this.x += 1;
-            }
-        }
-
-        if (command.equals("left")) {
-            if (direction.equals(MarsDirectionEnum.N.name())) {
-                this.direction = MarsDirectionEnum.W.name();
-            } else if (direction.equals(MarsDirectionEnum.W.name())) {
-                this.direction = MarsDirectionEnum.S.name();
-            } else if (direction.equals(MarsDirectionEnum.S.name())) {
-                this.direction = MarsDirectionEnum.E.name();
-            } else if (direction.equals(MarsDirectionEnum.E.name())) {
-                this.direction = MarsDirectionEnum.N.name();
-            }
-        }
-        if (command.equals("right")) {
-            if (direction.equals(MarsDirectionEnum.N.name())) {
-                this.direction = MarsDirectionEnum.E.name();
-            } else if (direction.equals(MarsDirectionEnum.E.name())) {
-                this.direction = MarsDirectionEnum.S.name();
-            } else if (direction.equals(MarsDirectionEnum.S.name())) {
-                this.direction = MarsDirectionEnum.W.name();
-            } else if (direction.equals(MarsDirectionEnum.W.name())) {
-                this.direction = MarsDirectionEnum.N.name();
-            }
-        }
-
     }
+
+    private void moveForward() {
+        if (direction.equals(MarsDirectionEnum.N.name())) {
+            this.y += 1;
+        } else if (direction.equals(MarsDirectionEnum.S.name())) {
+            this.y -= 1;
+        } else if (direction.equals(MarsDirectionEnum.E.name())) {
+            this.x += 1;
+        } else if (direction.equals(MarsDirectionEnum.W.name())) {
+            this.x -= 1;
+        }
+    }
+
+    private void moveBackward() {
+        if (direction.equals(MarsDirectionEnum.N.name())) {
+            this.y -= 1;
+        } else if (direction.equals(MarsDirectionEnum.S.name())) {
+            this.y += 1;
+        } else if (direction.equals(MarsDirectionEnum.E.name())) {
+            this.x -= 1;
+        } else if (direction.equals(MarsDirectionEnum.W.name())) {
+            this.x += 1;
+        }
+    }
+
+    private void turnLeft() {
+        if (direction.equals(MarsDirectionEnum.N.name())) {
+            this.direction = MarsDirectionEnum.W.name();
+        } else if (direction.equals(MarsDirectionEnum.W.name())) {
+            this.direction = MarsDirectionEnum.S.name();
+        } else if (direction.equals(MarsDirectionEnum.S.name())) {
+            this.direction = MarsDirectionEnum.E.name();
+        } else if (direction.equals(MarsDirectionEnum.E.name())) {
+            this.direction = MarsDirectionEnum.N.name();
+        }
+    }
+
+    private void turnRight() {
+        if (direction.equals(MarsDirectionEnum.N.name())) {
+            this.direction = MarsDirectionEnum.E.name();
+        } else if (direction.equals(MarsDirectionEnum.E.name())) {
+            this.direction = MarsDirectionEnum.S.name();
+        } else if (direction.equals(MarsDirectionEnum.S.name())) {
+            this.direction = MarsDirectionEnum.W.name();
+        } else if (direction.equals(MarsDirectionEnum.W.name())) {
+            this.direction = MarsDirectionEnum.N.name();
+        }
+    }
+
     public void executeCommands(java.util.List<String> commands) {
         commands.stream().forEach(this::move);
     }
+
     public void executeCommands(String commands) {
         java.util.Arrays.stream(commands.split(",")).map(String::trim).forEach(this::move);
     }
